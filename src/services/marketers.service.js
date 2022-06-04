60359fe604052ca85bd24e30c03003c48edc7e83
Victoria Akpan <veedahv55@gmail.com>
2022-06-04 22:06:29 +0100

    marketer service updated

--- src/services/marketers.service.js
+++ src/services/marketers.service.js
@@ -1,27 +1,28 @@
 import axios from 'axios';
+import store from '../store/index';
 import authHeader from './auth-header';
 import router from '../router';
 const API_URL = 'https://proguardpeercover.herokuapp.com/marketers/';
 class MarketerService {
   getAllMarketers() {
     return axios.get(API_URL, { headers: authHeader() }).catch((error) => {
-      if (error.response.status === 401) router.push({ name: 'Login' })
+      if (error.response.status === 401) store.dispatch("auth/logout")
     });
   }
   updateMarketer(marketer) {
     return axios.put(API_URL, marketer, { headers: authHeader() }).catch((error) => {
-      if (error.response.status === 401) router.push({ name: 'Login' })
+      if (error.response.status === 401) store.dispatch("auth/logout")
     });
   }
   createMarketer(marketer) {
     return axios.post(API_URL, marketer, { headers: authHeader() }).catch((error) => {
-      if (error.response.status === 401) router.push({ name: 'Login' })
+      if (error.response.status === 401) store.dispatch("auth/logout")
     });
   }
   getMarketer(MarketerId) {
     return axios.get(API_URL + MarketerId, { headers: authHeader() }).catch((error) => {
+      if (error.response.status === 401) store.dispatch("auth/logout")
       if (error.response.status === 404) router.push({ name: 'NotFound', params: { catchAll: '404' } })
-      if (error.response.status === 401) router.push({ name: 'Login' })
     });
   }
   delMarketer(MarketerId) {
